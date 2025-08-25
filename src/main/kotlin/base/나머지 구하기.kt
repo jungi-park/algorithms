2589427af67c71f607f0f168fe3f71739536d01a
박준기 <qmqqqqm@gmail.com>
2025-08-25 21:18:46 +0900

    코딩 기초 트레이닝 : 나머지 구하기 (solve)

---
+++ src/main/kotlin/base/나머지 구하기.kt
@@ -0,0 +1,15 @@
+package base
+
+class 나머지구하기Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = -1
+        answer = num1 % num2
+        return answer
+    }
+}
+
+// 이 솔루션 클래스를 테스트하기 위한 main 함수 예시
+fun main() {
+    val sol = 나머지구하기Solution().solution(3,2)
+    println(sol)
+}

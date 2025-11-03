d295317e538b16aa2ed407dfb88354426c3b46a1
박준기 <qmqqqqm@gmail.com>
2025-11-03 21:03:33 +0900

    코딩 모든 문제 : 한수 (solve) - 코드정리

--- src/main/kotlin/백준/practice/한수.kt
+++ src/main/kotlin/백준/practice/한수.kt
@@ -4,28 +4,22 @@ package 백준.practice
 fun main() {
     var answer = 0
     val N = readln().toInt()
-    outter@for (num in 1..N) {
+    outter@ for (num in 1..N) {
         val strNumArr = num.toString().map { it.digitToInt() }
 
-        val diffDefault = if (strNumArr.size == 1) {
-            0
-        } else {
-            strNumArr[1] - strNumArr[0]
-        }
-
-        if(strNumArr.size == 1){
+        if (strNumArr.size == 1) {
             ++answer
-        }else{
+        } else {
+            val diffDefault = strNumArr[1] - strNumArr[0]
             for (index in 0 until strNumArr.size - 1) {
                 val diff = strNumArr[index + 1] - strNumArr[index]
-                if(diffDefault != diff){
+                if (diffDefault != diff) {
                     continue@outter
                 }
             }
             ++answer
         }
     }
-
 
     println(answer)
 }
